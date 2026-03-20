3b32950525364fea54f1f6a7aec9ceebb0d4a483
hoangluong032002 <hoangluong032002@gmail.com>
2026-03-20 17:04:19 +0700

    Create Backend folder

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("Running...");
+});
+
+module.exports = app;
